bbadf93a243e11e7a59519289dc5530b7b79c7e6
Hannah <hannah.greenwell@gmail.com>
2018-11-05 16:54:25 +1100

    Added Game model and associations

--- pictionary/app/models/game.rb
+++ pictionary/app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  belongs_to :drawer, class_name: 'User', foreign_key: 'drawer_id'
+  belongs_to :guesser, class_name: 'User', foreign_key: 'guesser_id'
+
+  enum staus: ['waiting', 'playing', 'finished']
 end
--- pictionary/db/schema.rb
+++ pictionary/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_051237) do
+ActiveRecord::Schema.define(version: 2018_11_05_054639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2018_11_02_051237) do
     t.text "image"
     t.integer "user_id"
     t.integer "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer "drawer_id"
+    t.integer "guesser_id"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
